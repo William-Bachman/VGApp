9bd8cd32588091016160e3cf92aa9e63f89164b8
William-Bachman <william_bachman@outlook.com>
2015-04-27 11:15:54 -0400

    Fixed real time polling issue

--- Video Game project/VGScript.js
+++ Video Game project/VGScript.js
@@ -1,22 +1,33 @@
-﻿var firebaseURL = 'https://videogame.firebaseio.com/'
+﻿//noSQL database
+var firebaseURL = 'https://videogame.firebaseio.com/'
+//objConstructor
 var vgCharacter = function (Platform, Name, Story) {
     this.Platform = Platform;
     this.Name = Name;
     this.Story = Story;
 }
+//Just for fun on the prototype
+vgCharacter.prototype = function () {
+    alert("People who own" + this.Platform + "have enjoyed" + this.Name);
+}
+
+//vgCharacter array
 var vgCharacters = [];
 
+//sends characters to div and firebase
 var sendVGCharacter = function () {
     var Platform = $('#inputSystem').val();
     var Name = $('#inputName').val();
     var Story = $('#inputStory').val();
     var myCharacter = new vgCharacter(Platform, Name, Story);
     postVGCharacter(myCharacter);
+    //clears out the inputs
     $('#inputSystem').val('');
     $('#inputName').val('');
     $('#inputStory').val('');
 
 }
+//prints vg characters to the HTML
 var printVGcharacters = function () {
     $('#displayVGcharacters').html = ('');
     var elemString = '';
@@ -28,6 +39,7 @@ var printVGcharacters = function () {
     }
     $('#displayVGcharacters').html(elemString);
 }
+//Posts data from the inputs to the firebase
 var postVGCharacter = function (sendVGCharacter) {
     var request = new XMLHttpRequest();
     request.open('POST', firebaseURL + '.json', true);
@@ -47,8 +59,10 @@ var postVGCharacter = function (sendVGCharacter) {
 
 }
 
-var getVGCharacters = function (VGPoll) {
-    var request = new XMLHttpRequest();
+
+//this is the normal get call
+var getVGCharacters = function () {
+    var request = new XMLHttpRequest()
     request.open('GET', firebaseURL + '.json', true);
     request.onload = function () {
         if (this.status >= 200 && this.status < 400) {
@@ -57,17 +71,20 @@ var getVGCharacters = function (VGPoll) {
                 response[propName].key = propName;
                 vgCharacters.push(response[propName]);
             }
-
+           
             printVGcharacters();
         }
         else {
             console.log(this.response);
-            setTimeout(60000);
+           
         }
     }
     request.send();
+    
    
 }
+
+//enable the modal to trigger for allowing edits of the innerHTML objects
 var editVGCharacter = function (i) {
     $('#editPlatform').val(vgCharacters[i].Platform);
     $('#editName').val(vgCharacters[i].Name);
@@ -75,7 +92,7 @@ var editVGCharacter = function (i) {
     $('#SaveEditButton').html('<button type="button" class="btn btn-primary" data-dismiss="modal" onclick="saveEdit(' + i + ');">Save changes</button>')
     $('#myModal1').modal('toggle');
 }
-
+//Puts the updated information into firebase
 var putVGCharacter = function (data, i) {
     var request = new XMLHttpRequest();
     var key = vgCharacters[i].key;
@@ -92,6 +109,7 @@ var putVGCharacter = function (data, i) {
     }
     request.send(JSON.stringify(data));
 }
+//Saves the edited information into the HTML
 var saveEdit = function (i) {
     var Platform = $('#editPlatform').val();
     var Name = $('#editName').val();
@@ -99,6 +117,7 @@ var saveEdit = function (i) {
     var myCharacter = new vgCharacter(Platform, Name, Story);
     putVGCharacter(myCharacter, i);
 }
+//deletes the information out of the firebase
 var deleteVGCharacter = function (i) {
     var request = new XMLHttpRequest();
     request.open('DELETE', firebaseURL + vgCharacters[i].key + '.json', true);
@@ -106,7 +125,7 @@ var deleteVGCharacter = function (i) {
         if (this.status >= 200 && this.status < 400) {
             vgCharacters.splice(i, 1);
             printVGcharacters();
-            
+
 
         }
         else {
@@ -116,15 +135,35 @@ var deleteVGCharacter = function (i) {
     }
     request.send();
 }
+// This adds the reverse option to the string prototype (primative value)
 String.prototype.reverse = function () {
-    this.split("Reversed String").reverse().join("");
-    console.log("I'm a reversed string");
-};
-
-var VGPoll = function (){
-    $('#displayVGcharacters').val('');
-    setInterval(getVGCharacters, 5000);
+//split turns the string into an array of characters, reverse reverses these characters, join joins them back together in reverse order.
     
+     this.split("").reverse().join("");
 }
-VGPoll(getVGCharacters);
+//this is the polling get call
+var getVGCharacters2 = function () {
+    var request = new XMLHttpRequest()
+    setTimeout(getVGCharacters2, 3000);
+    request.open('GET', firebaseURL + '.json', true);
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            var response = JSON.parse(this.response);
+            
+            }
+
+        
+        else {
+            console.log(this.response);
+            clearTimeout(getvgCharacters2);
+        }
+    }
+    request.send();
+
+
+}
+
+   //this fetches the information from the Database.
 getVGCharacters();
+//this fires off the polling ajax call.
+getVGCharacters2();
